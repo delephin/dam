33e81b44c7bc95605f41a701b0c454367360acf7
delephin <delephin@gmail.com>
2022-04-20 19:51:26 -0300

    Adding docker-compose to load a mysql-server and phpmysql image, and dam.sql script to create the database and initialize it.

--- backend/db.js
+++ backend/db.js
@@ -4,7 +4,7 @@ var mysql = require('mysql');
 
 var configMysql = {
     connectionLimit: 10,
-    host: 'mysql-server',
+    host: 'localhost',
     port: 3306,
     user: 'root',
     password: 'userpass',
